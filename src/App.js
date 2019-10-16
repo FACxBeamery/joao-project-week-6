47381854187ff27548301841c7d4dc124130416e
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-10-16 10:58:09 +0100

    App component contionally renders depending on the state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import TriviaConfigForm from "./components/TriviaConfigForm";
+import Trivia from "./components/Trivia";
 
 function App() {
-	return <div className="App"></div>;
+	const [triviaState, setTriviaState] = useState("configuringForm");
+	const [configDetails, setConfigDetails] = useState({});
+	return (
+		<div className="App">
+			{triviaState === "configuringForm" ? (
+				<TriviaConfigForm
+					formState={0}
+					setTriviaState={setTriviaState}
+					setConfigDetails={setConfigDetails}
+				/>
+			) : null}
+			{triviaState === "startTrivia" ? <Trivia /> : null}
+		</div>
+	);
 }
 
 export default App;
