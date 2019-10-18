9dc590243073e806b90b9b53116902f5cfbd234a
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-10-18 10:05:27 +0100

    getquestions url refactoring conditionals Relates to #10

--- src/utils/getQuestions.js
+++ src/utils/getQuestions.js
@@ -1,12 +1,18 @@
 const getQuestions = ({ triviaCategory, triviaDifficulty }) => {
+	let categoryChosen = "";
+	let difficultyChosen = "";
+	if (triviaCategory !== "any") {
+		categoryChosen = `&category=${triviaCategory}`;
+	}
+	if (!(triviaCategory === "19" || triviaDifficulty === "any")) {
+		difficultyChosen = `&category=${triviaCategory}`;
+	}
+
 	return new Promise((resolve, reject) => {
 		fetch(
 			`https://opentdb.com/api.php?amount=20
-			${triviaCategory !== "any" ? `&category=${triviaCategory}` : ``}${
-				triviaCategory === "19" || triviaDifficulty === "any"
-					? ""
-					: `&difficulty=${triviaDifficulty}`
-			}&type=multiple&encode=url3986`
+			${categoryChosen}${difficultyChosen}
+			&type=multiple&encode=url3986`
 		)
 			.then((res) => resolve(res.json()))
 			.catch((err) => reject(err));
