3ec21177f55871fde3a9657553b32f36941a0d5b
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-10-18 09:29:39 +0100

    header with h1 tag and styling Relates to #9

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "./Header.module.css";
 
 const Header = ({ points }) => {
-	return <header className={styles.header}>Trivia craze</header>;
+	return (
+		<header className={styles.header}>
+			<h1 style={{ margin: 0, fontSize: "3rem" }}>Trivia craze</h1>
+		</header>
+	);
 };
 
 export default Header;
